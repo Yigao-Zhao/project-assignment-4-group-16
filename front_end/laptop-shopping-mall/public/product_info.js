67c69e33b0dfc5365e111a02376a66407303c1ff
Yigao-Zhao <yzha6372@uwo.ca>
2024-11-21 16:17:39 -0500

    resource

---
+++ front_end/laptop-shopping-mall/public/product_info.js
@@ -0,0 +1,82 @@
+/* Array for computer store products */
+
+const cart = [
+    {
+        productID: 1, 
+        productName: "Dell Inspiron 15 3520 Laptop", 
+        productType: "Laptop", 
+        productSpecifications: "Intel Core i5-1235U, 8GB DDR4 RAM, 256GB SSD, 15.6-inch FHD Display", 
+        productImage: 1,
+        productPrice: 379.99,
+        productStock: 10,
+    },
+    {
+        productID: 2, 
+        productName: "Lenovo ThinkPad X1 Carbon Gen 12", 
+        productType: "Laptop", 
+        productSpecifications: "Intel Core i7-1355U, 16GB LPDDR5 RAM, 512GB SSD, 14-inch WUXGA Touch Display", 
+        productImage: 3, 
+        productPrice: 1840.28,
+        productStock: 15,
+
+    },
+    {
+        productID: 3,
+        productName: "Razer Basilisk V3 Pro - Black",
+        productType: "Accessory",
+        productSpecifications: "Advanced gaming mouse with customizable buttons, Razer Chroma RGB, and HyperSpeed Wireless.",
+        productImage: 4,
+        productPrice: 129.99,
+        productStock: 25,
+        quantity: 0
+    },
+    {
+        productID: 4,
+        productName: "CHERRY XTRFY K5V2 Compact Black",
+        productType: "Accessory",
+        productSpecifications: "65% mechanical gaming keyboard with CHERRY MX2A Red switches, RGB lighting, and sound-dampening foam.",
+        productImage: 4,
+        productPrice: 139.00,
+        productStock: 20,
+        quantity: 0
+    },
+    {
+        productID: 5,
+        productName: "ASUS ROG Strix Go Gaming Headphones",
+        productType: "Accessory",
+        productSpecifications: "Wireless gaming headphones with USB-C 2.4 GHz adapter, AI noise-cancelling microphone, and multi-platform support.",
+        productImage: 5,
+        productPrice: 148.63,
+        productStock: 15,
+        quantity: 0
+    },
+    {
+        productID: 6,
+        productName: "HP-envy-laptop-17-cw1097nr",
+        productType: "Laptop",
+        productSpecifications: "Intel® Core™ Ultra 7, 32 GB RAM, 1 TB SSD, 17.3\" 4K UHD, NVIDIA® GeForce RTX™ 3050.",
+        productImage: 6, 
+        productPrice: 1449.99,
+        productStock: 10,
+
+    },
+    {
+        productID: 7,
+        productName: "lenovo-thinkpad-t490s-2",
+        productType: "Laptop",
+        productSpecifications: "i5 8250U 8th Gen, 16 GB RAM, 256 GB SSD, 14.1\" FHD, Intel HD Graphics.",
+        productImage: 7, 
+        productPrice: 439.00,
+        productStock: 15,
+
+    },
+    {
+        productID: 8,
+        productName: "Dell XPS 15 9530",
+        productType: "Laptop",
+        productSpecifications: "13th Gen Intel® Core™ i7-13620H, 32 GB DDR5 RAM, 1 TB SSD, 15.6\" FHD+, Intel® Arc™ Graphics A370M.",
+        productImage: 8, 
+        productPrice: 1909.99,
+        productStock: 12,
+    }
+]  
